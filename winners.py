9fe57c87b5f715cac0bde1576b2ff591376a1b0d
macloo <mmcadams@gmail.com>
2016-04-14 16:06:09 -0400

    rename files for test

--- winners.py
+++ winners.py
@@ -1,41 +1,70 @@
-# coding: utf-8
-# assignment 10: a small Flask app
-from flask import Flask, render_template
-from hugo_winners import HUGO_WINNERS
+import os
+import pymysql
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# location of my MySQL databases at PythonAnywhere
+basedir = 'macloo.mysql.pythonanywhere-services.com'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'mysql+pymysql://macloo:paTGIL2277@' + basedir + '/sockmarket'
+#   'mysql://username:password@localhost/db_name'
+#   note addition of +pymysql
+# also import pymysql
 
-@app.route('/')
-def home():
-    return '<h1>This is the home page.</h1><p>Go to /awards/</p>'
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# retrieve all the titles from the dataset and put them into a list
-def get_titles(source):
-    titles = []
-    for row in source:
-        title = row["Title"]
-        titles.append(title)
-    return sorted(titles)
 
-# find the row that matches the title in the URL, retrieve author and year
-def get_bookdata(source, title):
-    for row in source:
-        if title == row["Title"]:
-            # decode handles accented characters
-            author = row["Author"].decode('utf-8')
-            year = row["Year"]
-    return title, author, year
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
 
-@app.route('/awards/')
-def awards():
-    titles = get_titles(HUGO_WINNERS)
-    return render_template('awards.html', titles=titles)
+@app.route('/testdb/')
+def testdb():
+    if db.session.query('1').from_statement('SELECT 1').all():
+        return 'It works.'
+    else:
+        return 'Something is broken.'
 
-@app.route('/awards/<title>')
-def book(title):
-    title, author, year = get_bookdata(HUGO_WINNERS, title)
-    return render_template('book.html', title=title, author=author,
-    year=year)
+'''
+class Sock(db.Model):
+    __tablename__ = 'socks'
+    __table_args__ = {'autoload':True}
+    def __repr__(self):
+        # return '<Sock %r>' % self.name
+        return '<Sock %r>' % self.id
+'''
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+'''
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    given_id = "16"
+    id = Sock.query.filter_by(id=given_id)
+    if id is None:
+        session['known'] = False
+    else:
+        session['known'] = True
+        session['id'] = id
+    return render_template('index.html', name=session.get('name'),
+                           known=session.get('known', False))
+'''
+
+# if __name__ == '__main__':
+#     manager.run()
 # if __name__ == '__main__':
 #    app.run(debug=True)
---
+++ winners_BAK.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+# assignment 10: a small Flask app
+from flask import Flask, render_template
+from hugo_winners import HUGO_WINNERS
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>This is the home page.</h1><p>Go to /awards/</p>'
+
+# retrieve all the titles from the dataset and put them into a list
+def get_titles(source):
+    titles = []
+    for row in source:
+        title = row["Title"]
+        titles.append(title)
+    return sorted(titles)
+
+# find the row that matches the title in the URL, retrieve author and year
+def get_bookdata(source, title):
+    for row in source:
+        if title == row["Title"]:
+            # decode handles accented characters
+            author = row["Author"].decode('utf-8')
+            year = row["Year"]
+    return title, author, year
+
+@app.route('/awards/')
+def awards():
+    titles = get_titles(HUGO_WINNERS)
+    return render_template('awards.html', titles=titles)
+
+@app.route('/awards/<title>')
+def book(title):
+    title, author, year = get_bookdata(HUGO_WINNERS, title)
+    return render_template('book.html', title=title, author=author,
+    year=year)
+
+# if __name__ == '__main__':
+#    app.run(debug=True)
